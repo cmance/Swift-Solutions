12566c8b4bc95171eaa8b7e08d421ce7bae2ad35
cmance <cmance19@mail.wou.edu>
2024-02-20 17:13:30 -0800

    Created new seed script for new database entities

--- source/PopNGo/PopNGo/PopNGo/Data/Scripts/SEED.sql
+++ source/PopNGo/PopNGo/PopNGo/Data/Scripts/SEED.sql
@@ -1,14 +1,35 @@
--- Seed data for PG_User table
-INSERT INTO [PG_User] ([ASPNETUserID]) VALUES ('user1');
-INSERT INTO [PG_User] ([ASPNETUserID]) VALUES ('user2');
-INSERT INTO [PG_User] ([ASPNETUserID]) VALUES ('user3');
+-- Seed the PG_User table
+INSERT INTO [PG_User] ([ASPNETUserID])
+VALUES 
+('User1'),
+('User2'),
+('User3'),
+('User4'),
+('User5');
 
--- Seed data for Event table
-INSERT INTO [Event] ([EventID], [EventDate], [EventName], [EventDescription], [EventLocation]) VALUES ('event1', GETDATE(), 'Event 1', 'Event 1 Description', 'Location 1');
-INSERT INTO [Event] ([EventID], [EventDate], [EventName], [EventDescription], [EventLocation]) VALUES ('event2', GETDATE(), 'Event 2', 'Event 2 Description', 'Location 2');
-INSERT INTO [Event] ([EventID], [EventDate], [EventName], [EventDescription], [EventLocation]) VALUES ('event3', GETDATE(), 'Event 3', 'Event 3 Description', 'Location 3');
+-- Seed the Event table
+INSERT INTO [Event] ([EventID], [EventDate], [EventName], [EventDescription], [EventLocation])
+VALUES 
+('E1', '2022-12-01T00:00:00', 'Event 1', 'This is event 1', 'Location 1'),
+('E2', '2022-12-02T00:00:00', 'Event 2', 'This is event 2', 'Location 2'),
+('E3', '2022-12-03T00:00:00', 'Event 3', 'This is event 3', 'Location 3'),
+('E4', '2022-12-04T00:00:00', 'Event 4', 'This is event 4', 'Location 4'),
+('E5', '2022-12-05T00:00:00', 'Event 5', 'This is event 5', 'Location 5');
 
--- Seed data for EventHistory table
-INSERT INTO [EventHistory] ([UserID], [EventID], [ViewedDate]) VALUES (1, 1, GETDATE());
-INSERT INTO [EventHistory] ([UserID], [EventID], [ViewedDate]) VALUES (2, 2, GETDATE());
-INSERT INTO [EventHistory] ([UserID], [EventID], [ViewedDate]) VALUES (3, 3, GETDATE());
+-- Seed the FavoriteEvents table
+INSERT INTO [FavoriteEvents] ([UserID], [EventID])
+VALUES 
+(1, 1),  -- User 1 favorites Event 1
+(2, 2),  -- User 2 favorites Event 2
+(3, 3),  -- User 3 favorites Event 3
+(4, 4),  -- User 4 favorites Event 4
+(5, 5);  -- User 5 favorites Event 5
+
+-- Seed the EventHistory table
+INSERT INTO [EventHistory] ([UserID], [EventID], [ViewedDate])
+VALUES 
+(1, 1, '2022-12-01T00:00:00'),  -- User 1 viewed Event 1
+(2, 2, '2022-12-02T00:00:00'),  -- User 2 viewed Event 2
+(3, 3, '2022-12-03T00:00:00'),  -- User 3 viewed Event 3
+(4, 4, '2022-12-04T00:00:00'),  -- User 4 viewed Event 4
+(5, 5, '2022-12-05T00:00:00');  -- User 5 viewed Event 5
