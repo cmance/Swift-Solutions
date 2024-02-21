249e9335dacaef07c25a370f59feb3ef8db493cf
cmance <cmance19@mail.wou.edu>
2024-02-20 17:18:24 -0800

    Created new DOWN script for new DB entities

--- source/PopNGo/PopNGo/PopNGo/Data/Scripts/DOWN.sql
+++ source/PopNGo/PopNGo/PopNGo/Data/Scripts/DOWN.sql
@@ -1,10 +1,15 @@
 USE [PopNGoDB];
 
+-- Drop the foreign key constraints
 ALTER TABLE [EventHistory] DROP CONSTRAINT FK_EventHistory_UserID;
 ALTER TABLE [EventHistory] DROP CONSTRAINT FK_EventHistory_EventID;
+ALTER TABLE [FavoriteEvents] DROP CONSTRAINT FK_FavoriteEvents_UserID;
 
-DROP TABLE [PG_User];
+-- Drop the tables
+DROP TABLE IF EXISTS [FavoriteEvents];
+DROP TABLE IF EXISTS [EventHistory];
+DROP TABLE IF EXISTS [Event];
+DROP TABLE IF EXISTS [PG_User];
 
-DROP TABLE [EventHistory];
-
-DROP TABLE [Event];
+-- Drop the database
+DROP DATABASE IF EXISTS [PopNGoDB];
