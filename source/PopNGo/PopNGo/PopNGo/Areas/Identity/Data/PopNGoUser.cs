07d8f18461672c19a9a05cfdb133195c566e730f
Tristan Goucher <72034470+Fair-Strides@users.noreply.github.com>
2024-02-22 13:42:08 -0800

    Added Notifications page with inputs, default notification email, and migrations for the Identity model. (#36)

--- source/PopNGo/PopNGo/PopNGo/Areas/Identity/Data/PopNGoUser.cs
+++ source/PopNGo/PopNGo/PopNGo/Areas/Identity/Data/PopNGoUser.cs
@@ -8,4 +8,6 @@ public class PopNGoUser : IdentityUser
     public string FirstName { get; set; }
     [PersonalData]
     public string LastName { get; set; }
+    [PersonalData]
+    public string NotificationEmail { get; set; }
 }
