97ba286514de1259ad858f6eebe6ab579ee0633c
Cameron Mance <108043888+cmance@users.noreply.github.com>
2024-03-08 16:41:18 -0800

    Javascript refactoring (#54)
    * moved formatDateWithWeekday and formatHourMinute to formatStartTime.js
    
    * Moved toast functionality
    
    * Moved show toast and imported into file

--- source/PopNGo/PopNGo/PopNGo/wwwroot/js/util/formatStartTime.js
+++ source/PopNGo/PopNGo/PopNGo/wwwroot/js/util/formatStartTime.js
@@ -1,6 +1,14 @@
-function formatStartTime(dateString) {
+export function formatStartTime(dateString) {
     const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' };
     return new Date(dateString).toLocaleDateString(undefined, options);
 }
 
-export { formatStartTime };
+export function formatDateWithWeekday(startTime) {
+    const date = new Date(startTime);
+    return date.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' });
+}
+
+export function formatHourMinute(startTime) {
+    const date = new Date(startTime);
+    return date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' });
+}
