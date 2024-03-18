cf8b2deb99f000a4a01e6d34632a142167cadc5a
Cameron Mance <ccmance@icloud.com>
2024-03-17 17:42:28 -0700

    Fix for map page scrolling

--- source/PopNGo/PopNGo/PopNGo/wwwroot/js/util/mapFetching.js
+++ source/PopNGo/PopNGo/PopNGo/wwwroot/js/util/mapFetching.js
@@ -11,7 +11,7 @@ let isWaiting = false;
  * @param {any} start - Event offset (should be page * items per page)
  * @returns
  */
-export function updateLocationAndFetch(map, start) {
+export function updateLocationAndFetch(map, start = 0) {
     if (isWaiting || isCurrentlySearching()) return;
     isWaiting = true;
 
