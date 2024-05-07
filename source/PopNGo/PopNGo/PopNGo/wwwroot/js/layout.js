532b91d07d36129b965c0b1dea549d4d3663b793
Cameron Mance <ccmance@icloud.com>
2024-05-07 00:32:31 -0700

    Removed color staying on icon click on navbar

--- source/PopNGo/PopNGo/PopNGo/wwwroot/js/layout.js
+++ source/PopNGo/PopNGo/PopNGo/wwwroot/js/layout.js
@@ -29,22 +29,22 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-document.querySelectorAll('.onhover svg').forEach(function (svg, index) {
-    svg.addEventListener('click', function (event) {
-        // Remove the 'clicked' class from all SVGs
-        document.querySelectorAll('.onhover svg.clicked').forEach(function (clickedSvg) {
-            clickedSvg.classList.remove('clicked');
-        });
+// document.querySelectorAll('.onhover svg').forEach(function (svg, index) {
+//     svg.addEventListener('click', function (event) {
+//         // Remove the 'clicked' class from all SVGs
+//         document.querySelectorAll('.onhover svg.clicked').forEach(function (clickedSvg) {
+//             clickedSvg.classList.remove('clicked');
+//         });
 
-        // Add the 'clicked' class to the clicked SVG
-        this.classList.add('clicked');
+//         // Add the 'clicked' class to the clicked SVG
+//         this.classList.add('clicked');
 
-        // Store the index of the clicked SVG in sessionStorage
-        sessionStorage.setItem('clickedSvgIndex', index);
-    });
+//         // Store the index of the clicked SVG in sessionStorage
+//         sessionStorage.setItem('clickedSvgIndex', index);
+//     });
 
-    // If this SVG was the last one clicked before the page refresh, add the 'clicked' class to it
-    if (sessionStorage.getItem('clickedSvgIndex') == index) {
-        svg.classList.add('clicked');
-    }
-});
+//     // If this SVG was the last one clicked before the page refresh, add the 'clicked' class to it
+//     if (sessionStorage.getItem('clickedSvgIndex') == index) {
+//         svg.classList.add('clicked');
+//     }
+// });
